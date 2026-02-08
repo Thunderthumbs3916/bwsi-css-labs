715cd8b8e65ea0f9370df5a3ad24b0643d39168b
Kush Sethia <kushsethia09@gmail.com>
2026-02-08 12:54:33 -0500

    added introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Kush Sethia" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi, I'm Kush Sethia, a junior from New Jersey, although I spent the first 15 years of my life in Mumbai, India. I am interested in making and coding robots and everything related, currently I make RC planes and coding stabilizers for them. My favorite sport is Archery, and am actually a nationally recognized player in India. Looking forward to this year's courses and working with new people!")
 
 if __name__ == "__main__":
     main()
